869ea13c27588772a484b4454148d8ff84aaa1ef
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-07-07 19:49:24 +0700

    berkas: add pdf viewer

---
+++ resources/views/berkas/pdf_viewer.blade.php
@@ -0,0 +1,88 @@
+@extends('layout.template')
+
+@section('content')
+
+
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <iframe src="{{ $myFile }}" width="100%" height="600px"></iframe>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
+@endsection
+
+@section('script')
+
+<link rel="stylesheet" href="{{ asset('template/') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ asset('template/') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ asset('template/') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('template/') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/jszip/jszip.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{ asset('template/') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('template/') }}/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('template/') }}/dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+  $(function() {
+    $("#example1").DataTable({
+      "responsive": true,
+      "lengthChange": false,
+      "autoWidth": false,
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+
+<script type="text/javascript">
+  $('.confirm-delete').click(function(event) {
+    var form = $(this).closest("form");
+    var name = $(this).data("name");
+    event.preventDefault();
+    Swal.fire({
+      title: `Apakah Anda yakin menghapus berkas ini ?`,
+      text: "Data akan terhapus",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        form.submit();
+      }
+    });
+  });
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,34 +21,35 @@ Route::post('/register_data', [App\Http\Controllers\UserController::class, 'regi
 
 Route::group(['middleware' => ['auth']], function () {
 
-    Route::group(['prefix' => 'user', 'middleware' => ['rolecheck:3']], function () {
+  Route::group(['prefix' => 'user', 'middleware' => ['rolecheck:3']], function () {
 
-        Route::get('/', [App\Http\Controllers\UserController::class, 'index'])->name('read_user');
-        Route::get('/detail', [App\Http\Controllers\UserController::class, 'detailUser'])->name('detail_user');
-        Route::get('/edit_page', [App\Http\Controllers\UserController::class, 'editUserPage'])->name('edit_user_page');
-        Route::post('/edit_data', [App\Http\Controllers\UserController::class, 'editUserData'])->name('edit_user_data');
-        Route::post('/change_status', [App\Http\Controllers\UserController::class, 'changeStatus'])->name('change_user_status');
-        Route::post('/reset_password', [App\Http\Controllers\UserController::class, 'resetPassword'])->name('reset_user_password');
-    });
+    Route::get('/', [App\Http\Controllers\UserController::class, 'index'])->name('read_user');
+    Route::get('/detail', [App\Http\Controllers\UserController::class, 'detailUser'])->name('detail_user');
+    Route::get('/edit_page', [App\Http\Controllers\UserController::class, 'editUserPage'])->name('edit_user_page');
+    Route::post('/edit_data', [App\Http\Controllers\UserController::class, 'editUserData'])->name('edit_user_data');
+    Route::post('/change_status', [App\Http\Controllers\UserController::class, 'changeStatus'])->name('change_user_status');
+    Route::post('/reset_password', [App\Http\Controllers\UserController::class, 'resetPassword'])->name('reset_user_password');
+  });
 
-    Route::get('/setup_profile', [App\Http\Controllers\UserController::class, 'setup_profile'])->name('setup_profile');
-    Route::post('/setup_profile_data', [App\Http\Controllers\UserController::class, 'setup_profile_data'])->name('setup_profile_data');
-    Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
+  Route::get('/setup_profile', [App\Http\Controllers\UserController::class, 'setup_profile'])->name('setup_profile');
+  Route::post('/setup_profile_data', [App\Http\Controllers\UserController::class, 'setup_profile_data'])->name('setup_profile_data');
+  Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
 
-    Route::group(['prefix' => 'berkas'], function () {
+  Route::group(['prefix' => 'berkas'], function () {
 
-        Route::get('/', [App\Http\Controllers\BerkasController::class, 'index'])->name('dashboard');
-        Route::get('/read_berkas', [App\Http\Controllers\BerkasController::class, 'readData'])->name('read_berkas');
-        Route::get('/choose_category', [App\Http\Controllers\BerkasController::class, 'chooseCategory'])->name('choose_category');
-        Route::get('/create_berkas', [App\Http\Controllers\BerkasController::class, 'createBerkasPage'])->name('create_berkas_page');
-        Route::post('/create_berkas_data', [App\Http\Controllers\BerkasController::class, 'createBerkasData'])->name('create_berkas_data');
-        Route::get('/detail_data', [App\Http\Controllers\BerkasController::class, 'detailData'])->name('detail_data_berkas');
-        Route::get('/download', [App\Http\Controllers\BerkasController::class, 'downloadFile'])->name('download_file');
-        Route::get('/edit_berkas_page', [App\Http\Controllers\BerkasController::class, 'editBerkasPage'])->name('edit_berkas_page');
-        Route::post('/edit_data', [App\Http\Controllers\BerkasController::class, 'editBerkasData'])->name('edit_berkas_data');
-        Route::post('/delete_berkas', [App\Http\Controllers\BerkasController::class, 'deleteData'])->name('delete_data_berkas');
-        Route::get('/validate_berkas', [App\Http\Controllers\BerkasController::class, 'validateBerkas'])->name('validate_berkas');
-        Route::get('/approved_berkas', [App\Http\Controllers\BerkasController::class, 'approvedBerkas'])->name('approved_berkas');
-        Route::get('/unapproved_berkas', [App\Http\Controllers\BerkasController::class, 'unApprovedBerkas'])->name('unapproved_berkas');
-    });
+    Route::get('/', [App\Http\Controllers\BerkasController::class, 'index'])->name('dashboard');
+    Route::get('/read_berkas', [App\Http\Controllers\BerkasController::class, 'readData'])->name('read_berkas');
+    Route::get('/choose_category', [App\Http\Controllers\BerkasController::class, 'chooseCategory'])->name('choose_category');
+    Route::get('/create_berkas', [App\Http\Controllers\BerkasController::class, 'createBerkasPage'])->name('create_berkas_page');
+    Route::post('/create_berkas_data', [App\Http\Controllers\BerkasController::class, 'createBerkasData'])->name('create_berkas_data');
+    Route::get('/detail_data', [App\Http\Controllers\BerkasController::class, 'detailData'])->name('detail_data_berkas');
+    Route::get('/download', [App\Http\Controllers\BerkasController::class, 'downloadFile'])->name('download_file');
+    Route::get('/edit_berkas_page', [App\Http\Controllers\BerkasController::class, 'editBerkasPage'])->name('edit_berkas_page');
+    Route::post('/edit_data', [App\Http\Controllers\BerkasController::class, 'editBerkasData'])->name('edit_berkas_data');
+    Route::post('/delete_berkas', [App\Http\Controllers\BerkasController::class, 'deleteData'])->name('delete_data_berkas');
+    Route::get('/validate_berkas', [App\Http\Controllers\BerkasController::class, 'validateBerkas'])->name('validate_berkas');
+    Route::get('/approved_berkas', [App\Http\Controllers\BerkasController::class, 'approvedBerkas'])->name('approved_berkas');
+    Route::get('/unapproved_berkas', [App\Http\Controllers\BerkasController::class, 'unApprovedBerkas'])->name('unapproved_berkas');
+    Route::get('/view_file', [App\Http\Controllers\BerkasController::class, 'viewFile'])->name('view_file');
+  });
 });
